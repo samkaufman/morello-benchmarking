8bc76993d84ffd2320f42b0d05938b283239900f
Sam Kaufman <emrysk@gmail.com>
2024-02-12 13:19:59 -0800

    Larger benchmarks & remove test- prefix.

--- make_config.sh
+++ make_config.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
 set -e
 
-declare -a matmul_sizes=("8")
+declare -a morello_matmul_sizes=("8")
+declare -a matmul_sizes=("8" "64" "128" "256" "512" "1024" "2048")
 
 # Get the latest commit hash of the main branch
 declare -r MORELLO_HASH=$(
     curl -s "https://api.github.com/repos/samkaufman/morello/branches/main" |
     jq -r '.commit.sha')
 
-for i in "${matmul_sizes[@]}"; do
+for i in "${morello_matmul_sizes[@]}"; do
 echo '[[jobs]]'
 echo 'name = "matmul"'
 echo "size = $i"
 echo 'batch_size = 1'
-echo "backend_name = \"test-morello\""
+echo "backend_name = \"morello\""
 echo "docker_path = \"./morello\""
 echo "docker_build_args = { MORELLO_VERSION = \"$MORELLO_HASH\" }"
 echo "command = [ \"matmul\", \"$i\" ]"
 echo ""
- 
+done
+
+for i in "${matmul_sizes[@]}"; do
 echo '[[jobs]]'
 echo 'name = "matmul-u8s8s16os16"'
 echo "size = $i"
 echo 'batch_size = 1'
-echo 'backend_name = "test-aocl"'
+echo 'backend_name = "aocl"'
 echo 'docker_path = "./aocl"'
 echo "command = [ \"$i\" ]"
 echo ""
@@ -33,7 +36,7 @@ for b in "tvm" "eigen"; do
     echo 'name = "matmul"'
     echo "size = $i"
     echo 'batch_size = 1'
-    echo "backend_name = \"test-$b\""
+    echo "backend_name = \"$b\""
     echo "docker_path = \"./$b\""
     echo "command = [ \"$i\" ]"
     echo ""
