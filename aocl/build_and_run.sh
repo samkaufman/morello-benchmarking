4e4cac0aebeab6780945e54bbc380ece544505cf
Sam Kaufman <emrysk@gmail.com>
2024-12-29 10:44:47 -0800

    upgrade Rust, Clang, and Ubuntu

--- aocl/build_and_run.sh
+++ aocl/build_and_run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e 
-clang++-17 -std=c++17 -O3 -march=core-avx2 "-DPROBLEM_SIZE=$1" -DNDEBUG \
+clang++-18 -std=c++17 -O3 -march=core-avx2 "-DPROBLEM_SIZE=$1" -DNDEBUG \
     -I/opt/aocl/include/blis \
     -L/opt/aocl/lib -lblis-mt -lm -fopenmp \
     -o aocl_bench aocl_bench.cpp
--- eigen/build_and_run.sh
+++ eigen/build_and_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e 
-clang++-17 -std=c++17 -O3 -march=core-avx2 "-DPROBLEM_SIZE=$1" -DNDEBUG \
+clang++-18 -std=c++17 -O3 -march=core-avx2 "-DPROBLEM_SIZE=$1" -DNDEBUG \
     -I/eigen -o eigen_bench eigen_bench.cpp
 ./eigen_bench
