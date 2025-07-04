318096792a30d53c4d9a037433bf9e8a934abd4f
Sam Kaufman <emrysk@gmail.com>
2025-07-04 11:30:09 -0700

    fix for new tvm api

--- tvm/bench_tvm.py
+++ tvm/bench_tvm.py
@@ -40,15 +40,15 @@ def main():
 
     a = tvm.nd.array(numpy.random.rand(1, m, k).astype(DTYPE), device)
     b = tvm.nd.array(numpy.random.rand(1, k, n).astype(DTYPE), device)
-    # c = tvm.nd.array(numpy.zeros((m, n), dtype=DTYPE), device)
+    c = tvm.nd.array(numpy.zeros((1, m, n), dtype=DTYPE), device)
 
     # TODO: Check function correctness.
 
-    func(a, b)  # Warm-up
+    func(a, b, c)  # Warm-up
     for _ in range(outer_steps):
         start = time.time()
         for _ in range(inner_steps):
-            func(a, b)
+            func(a, b, c)
         duration = time.time() - start
         print(f"{duration:.10f}s")
 
@@ -57,7 +57,7 @@ def main():
     #   and https://tvm.apache.org/docs/reference/api/python/runtime.html#tvm.runtime.Module.time_evaluator.
     # TODO: Add a CLI argument for using the TVM evalautor.
     evaluator = func.time_evaluator(func.entry_name, device, number=inner_steps)
-    print("TVM time evaluator: %fs" % evaluator(a, b).mean, file=sys.stderr)
+    print("TVM time evaluator: %fs" % evaluator(a, b, c).mean, file=sys.stderr)
 
 
 def make_net(m, k, n):
@@ -65,51 +65,11 @@ def make_net(m, k, n):
     B = te.placeholder((1, k, n), dtype=DTYPE, name="B")
 
     with tvm.target.Target(TARGET):
-        s = topi.nn.batch_matmul(A, B)
+        C = topi.nn.batch_matmul(A, B, transpose_a=False, transpose_b=False)
 
-    func = tvm.build(s, [A, B], target=TARGET, name="mmult")
-    lowered = tvm.lower(s, [A, B], simple_mode=True)
-    return func, lowered
-
-    # -----------
-    # TODO: Delete the below unreachable code.
-    # -----------
-
-    # Algorithm
-    k_reduction = te.reduce_axis((0, k), "k")
-    A = te.placeholder((m, k), dtype=DTYPE, name="A")
-    B = te.placeholder((k, n), dtype=DTYPE, name="B")
-    packedB = te.compute(
-        (n / BN, k, BN),
-        lambda bigN, k, littleN: B[k, bigN * BN + littleN],
-        name="packedB",
-    )
-    C = te.compute(
-        (m, n),
-        lambda m, n: te.sum(
-            A[m, k] * packedB[n // BN, k, tvm.tir.indexmod(n, BN)], axis=k_reduction
-        ),
-        name="C",
-    )
-
-    s = te.create_schedule(C.op)
-    CC = s.cache_write(C, "global")
-    mo, no, mi, ni = s[C].tile(C.op.axis[0], C.op.axis[1], BN, BN)
-    s[CC].compute_at(s[C], no)
-
-    mc, nc = s[CC].op.axis
-
-    (kaxis,) = s[CC].op.reduce_axis
-    ko, ki = s[CC].split(kaxis, factor=K_FACTOR)
-    s[CC].reorder(ko, mc, ki, nc)
-    s[CC].vectorize(nc)
-    s[CC].unroll(ki)
-
-    _, _, littleN = s[packedB].op.axis
-    s[packedB].vectorize(littleN)
-
-    func = tvm.build(s, [A, B, C], target=TARGET, name="mmult")
-    lowered = tvm.lower(s, [A, B, C], simple_mode=True)
+    # Create PrimFunc using the modern TensorIR approach
+    func = tvm.build(te.create_prim_func([A, B, C]), target=TARGET)
+    lowered = te.create_prim_func([A, B, C])
     return func, lowered
 
 
