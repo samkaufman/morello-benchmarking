c7ced5d1868ad140dfa3d115463254c1d6dc7d41
Sam Kaufman <emrysk@gmail.com>
2025-07-07 18:43:09 -0700

    pass through `--filter` (and other args) to cherrybench from run.sh

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ tmpdir="$(mktemp -d)"
 ./make_config.sh > "$tmpdir/config.toml"
 
 venv="$(poetry show -C ./cherrybench -v 2>&1 | grep --color=never "Using virtualenv:" | cut -c19-)"
-sudo "$venv/bin/python" -m cherrybench "$tmpdir/config.toml"
+sudo "$venv/bin/python" -m cherrybench "$tmpdir/config.toml" "$@"
